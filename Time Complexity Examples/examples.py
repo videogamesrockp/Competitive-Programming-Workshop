4fa91d8123ad2a1038a7371cb1d11449441ddbba
Aster <aster.deng@gmail.com>
2024-07-02 14:32:36 -0700

    Add fijle

---
+++ Time Complexity Examples/examples.py
@@ -0,0 +1,35 @@
+# Example 1
+for i in range(n):
+    print(i)
+
+for i in range(n):
+    print(i)
+
+# Example 2
+for i in range(n):
+    if arr[i] == 10:
+        print(i)
+        break
+
+# Example 3
+for i in range(n):
+    for j in range(m):
+        print(i, j)
+
+# Example 4
+for i in range(m):
+    print(i)
+
+for i in range(n):
+    print(i)
+
+# Example 5
+print(n + m)
+
+# Example 6
+for i in range(n):
+    for j in range(n):
+        print(i, j)
+
+for i in range(n):
+    print(i)
